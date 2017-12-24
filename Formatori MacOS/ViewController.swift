954ebac9317816024809a040866f23c17ed2eb2c
Dani Tox <DaniTox@users.noreply.github.com>
2017-12-24 10:19:14 +0100

    Initial Commit

---
+++ Formatori MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Formatori MacOS
+//
+//  Created by Dani Tox on 24/12/17.
+//  Copyright © 2017 Dani Tox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
